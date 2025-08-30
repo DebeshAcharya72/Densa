e6d5b299270060418671ec1bf757f35d1e0a738d
DebeshAcharya72 <debeshacharya72@gmail.com>
2025-08-30 21:49:59 +0530

    main changes done

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { ThemeProvider } from "@mui/material/styles";
-import Theme from "./theme/Theme";
+import Theme from "./theme/Theme.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
